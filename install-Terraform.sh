b54154f33c6f28184cf96d9446a5ab490fc47cfa
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-07 17:16:33 +0100

    Update install-Terraform.sh with Terraform and AWS CLI
    Added installation steps for Terraform and AWS CLI.

--- install-Terraform.sh
+++ install-Terraform.sh
@@ -1,9 +1,11 @@
 https://developer.hashicorp.com/terraform/install
-------------Ubuntu-----------------------------
+--------------- Terraform-----------------------------
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 terraform --version
+----------------------------------------------
+https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 --------------AWS Cli-------------------------
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
