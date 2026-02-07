1bdd93af9a0333c6949a6adbeafe900ae7fd0bd9
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-07 17:15:47 +0100

    Update install-Terraform.sh

--- install-Terraform.sh
+++ install-Terraform.sh
@@ -4,3 +4,10 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 terraform --version
+--------------AWS Cli-------------------------
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+aws configure
+-----------------------------------------------
