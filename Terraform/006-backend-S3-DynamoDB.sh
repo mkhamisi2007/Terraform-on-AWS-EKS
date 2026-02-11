aa5cf0fabaed3ad541d31fb620a4270afdf6ed61
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-11 09:41:15 +0100

    Update 006-backend-S3-DynamoDB.sh

--- Terraform/006-backend-S3-DynamoDB.sh
+++ Terraform/006-backend-S3-DynamoDB.sh
@@ -7,4 +7,12 @@ aws dynamodb create-table \
 #use 
 terraform init -reconfigure 
 terraform plan
-# record will inserted in DynamoDB (mohammad-khamisi-us-bucket/dev2/terraform.tfstate - {"ID":"822cb1a5-adb0-4d3a-43f5-420dd6f3631f","Operation":"OperationTypePlan","Info":"","Who":"KHAMISI\\Administrateur@KHAMISI","Version":"1.14.4","Created":"2026-02-11T08:36:46.7714652Z","Path":"mohammad-khamisi-us-bucket/dev2/terraform.tfstate"}
+# record will inserted in DynamoDB when start plan 
+# record will deleted after finish plan
+# mohammad-khamisi-us-bucket/dev2/terraform.tfstate - {"ID":"822cb1a5-adb0-4d3a-43f5-420dd6f3631f","Operation":"OperationTypePlan","Info":"","Who":"KHAMISI\\Administrateur@KHAMISI","Version":"1.14.4","Created":"2026-02-11T08:36:46.7714652Z","Path":"mohammad-khamisi-us-bucket/dev2/terraform.tfstate"}
+
+
+
+
+
+
