4b498d06789de140f6113c68a4518c5155881224
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-11 11:34:15 +0100

    Create 007-providers.sh

---
+++ Terraform/007-providers.sh
@@ -0,0 +1,35 @@
+clouds provider , kubernetes , Active Directory , DNS , ....
+---------------------------------------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# (برای مثال) اگر کلاستر K8s داری، helm باید بداند به کدام کلاستر وصل شود
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+-------------------------------
+resource "aws_security_group" "example" {
+  name = "example-sg"
+}
+
+resource "helm_release" "nginx" {
+  name       = "nginx"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx"
+}
