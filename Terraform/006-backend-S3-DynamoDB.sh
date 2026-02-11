40628cafabc2e5c6f2e906d332086c4f4e21733d
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-11 09:51:36 +0100

    Update 006-backend-S3-DynamoDB.sh

--- Terraform/006-backend-S3-DynamoDB.sh
+++ Terraform/006-backend-S3-DynamoDB.sh
@@ -36,8 +36,8 @@ terraform plan
 # mohammad-khamisi-us-bucket/dev2/terraform.tfstate - {"ID":"822cb1a5-adb0-4d3a-43f5-420dd6f3631f","Operation":"OperationTypePlan","Info":"","Who":"KHAMISI\\Administrateur@KHAMISI","Version":"1.14.4","Created":"2026-02-11T08:36:46.7714652Z","Path":"mohammad-khamisi-us-bucket/dev2/terraform.tfstate"}
 
 terraform apply
-# record will inserted in DynamoDB when start plan 
-# record will deleted after finish plan
+# record will inserted in DynamoDB when start apply 
+# record will deleted after finish apply
 # "terraform.tfstate" file will copy in S3
 
 
