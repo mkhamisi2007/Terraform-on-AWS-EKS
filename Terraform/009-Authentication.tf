430ef6200919c6afba583449f5794dc358aaed38
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-16 16:25:31 +0100

    Add AWS CLI configuration commands
    Add AWS configuration commands to the authentication file

--- Terraform/009-Authentication.tf
+++ Terraform/009-Authentication.tf
@@ -8,7 +8,12 @@ export AWS_ACCESS_KEY_ID="..."
 export AWS_SECRET_ACCESS_KEY="..."
 export AWS_SESSION_TOKEN="..."   
 export AWS_REGION="eu-west-3"
--------------------------------------------------------
+------------------------------------------------------- aws configure
+aws configure
+cat ~/.aws/credentials
+[default]
+aws_access_key_id = XXXXXXXXXXXXXXXXXXX
+aws_secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 -------------------------------------------------------
 -------------------------------------------------------
 -------------------------------------------------------
