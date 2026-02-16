221909eaa51bd0ab60085c132249d48b2ba76fb0
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-16 16:30:22 +0100

    Update 009-Authentication.tf

--- Terraform/009-Authentication.tf
+++ Terraform/009-Authentication.tf
@@ -1,3 +1,4 @@
+------------------------------------------------------- direct
 provider "aws" {
   region     = "us-east-1"
   access_key = "value"
@@ -13,8 +14,17 @@ aws configure
 cat ~/.aws/credentials
 [default]
 aws_access_key_id = XXXXXXXXXXXXXXXXXXX
-aws_secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
--------------------------------------------------------
--------------------------------------------------------
--------------------------------------------------------
+aws_secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+------------------------------------------------------- Loging in spacial profile
+aws sso login --profile my-sso
+------------------------------------------------------- Assume Role
+provider "aws" {
+  region  = "eu-west-3"
+  profile = "base-user"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::123456789012:role/TerraformRole"
+    session_name = "terraform"
+  }
+}
 -------------------------------------------------------
