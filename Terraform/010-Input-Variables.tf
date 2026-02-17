c06701d8951930fde92f73e6d8449e3f8c06513c
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-17 10:43:57 +0100

    Update 010-Input-Variables.tf

--- Terraform/010-Input-Variables.tf
+++ Terraform/010-Input-Variables.tf
@@ -8,6 +8,9 @@ instance_type = "t3.micro"
 ---
 terraform apply -var-file="any-name.tfvars" #--> OR Default one (terraform.tfvars)
 -----------------------------------------------------------
+# Automaticly uploded
+*.auto.tfvars
+-----------------------------------------------------------
 variable "region" {
   description = "AWS region"
   type        = string
