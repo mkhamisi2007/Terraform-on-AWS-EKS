90bbbdcce30fd2bdb92b6b9b471c824816e4fbaa
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-17 16:17:06 +0100

    Update use-another-teraformstate-data.tf

--- Terraform/data/use-another-teraformstate-data.tf
+++ Terraform/data/use-another-teraformstate-data.tf
@@ -7,3 +7,7 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+------------------------------------Use it in another terraform project -------------------------------
+resource "aws_instance" "web" {
+  subnet_id = data.terraform_remote_state.vpc.outputs.public_subnet_id
+}
