9ff4a9ba1e0294bfbd3a62e65bbe94bae32ede8e
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-17 16:24:08 +0100

    Update use-another-teraformstate-data.tf

--- Terraform/data/use-another-teraformstate-data.tf
+++ Terraform/data/use-another-teraformstate-data.tf
@@ -12,7 +12,18 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+--------------------------------------Or if it is in S3 backend---------------------- import from S3
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-on-aws-for-ec2"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 ---------------------------------------Use it---------------------------------------
 resource "aws_instance" "web" {
   subnet_id = data.terraform_remote_state.vpc.outputs.public_subnet_id
 }
+
