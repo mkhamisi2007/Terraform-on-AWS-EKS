e9ff09e73830120f06a55dbdf5c3aa2bcb7c6686
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-17 10:45:31 +0100

    Update 010-Input-Variables.tf

--- Terraform/010-Input-Variables.tf
+++ Terraform/010-Input-Variables.tf
@@ -2,11 +2,11 @@ terraform apply -var="region=us-east-1"
 -----------------------------------------------------------
 export TF_VAR_region="us-east-1"
 -----------------------------------------------------------
-# any-name.tfvars Or terraform.tfvars
+# any-name.tfvars OR Default one (terraform.tfvars) without -var
 region = "us-east-1"
 instance_type = "t3.micro"
 ---
-terraform apply -var-file="any-name.tfvars" #--> OR Default one (terraform.tfvars)
+terraform apply -var-file="any-name.tfvars" 
 -----------------------------------------------------------
 # Automaticly uploded
 *.auto.tfvars
