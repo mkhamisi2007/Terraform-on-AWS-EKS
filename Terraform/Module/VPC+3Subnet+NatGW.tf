55a48bbf67e8faff49ba6cf3a3d3a29bca12c14b
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-20 17:18:46 +0100

    Update VPC+3Subnet+NatGW.tf

--- Terraform/Module/VPC+3Subnet+NatGW.tf
+++ Terraform/Module/VPC+3Subnet+NatGW.tf
@@ -1,3 +1,6 @@
+data "aws_availability_zones" "availability_zones" { #--------> it give us a list of AZ
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.6.0"
@@ -6,6 +9,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
+  # OR 
+  #azs             = data.aws_availability_zones.availability_zones.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
