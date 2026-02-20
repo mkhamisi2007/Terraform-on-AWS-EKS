81a23e275f372808faa8f99e17e0ce28fcf0352b
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-20 17:03:52 +0100

    Update exec-commonds-on-EC2.tf

--- Terraform/File-Provisioner/exec-commonds-on-EC2.tf
+++ Terraform/File-Provisioner/exec-commonds-on-EC2.tf
@@ -24,3 +24,26 @@ resource "aws_instance" "web" {
     }
   }
 }
+----------------------------------------------------- OR file.sh
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-xxxxxxxx"   # Ubuntu AMI
+  instance_type = "t3.micro"
+  key_name      = "my-key"
+
+  vpc_security_group_ids = ["sg-xxxxxxxx"]
+
+  provisioner "remote-exec" {
+    script = "install.sh"
+  
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("my-key.pem")
+      host        = self.public_ip
+    }
+  }
+}
