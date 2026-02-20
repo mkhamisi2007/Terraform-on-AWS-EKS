f3be3ed957d4066394d4d30838ad6f12eaecbcea
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-20 17:29:29 +0100

    Update 012-null-resource.tf

--- Terraform/012-null-resource.tf
+++ Terraform/012-null-resource.tf
@@ -30,3 +30,37 @@ resource "null_resource" "after_instance" {
     command = "echo Instance created!"
   }
 }
+--------------------------------------------------use with File provision
+resource "aws_instance" "demo" {
+  ami           = "ami-xxxxxxxx"
+  instance_type = "t3.micro"
+  key_name      = "my-key"
+}
+
+resource "null_resource" "deploy_script" {
+
+  depends_on = [aws_instance.demo]
+
+  triggers = {
+    instance_id = aws_instance.demo.id
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("my-key.pem")
+    host        = aws_instance.demo.public_ip
+  }
+
+  provisioner "file" {
+    source      = "app.sh"
+    destination = "/home/ubuntu/app.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/app.sh",
+      "/home/ubuntu/app.sh"
+    ]
+  }
+}
