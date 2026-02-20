4626d9caf233d0485d36b236ad3e2f7f4397bd63
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-20 17:10:00 +0100

    Update local-exec.tf

--- Terraform/File-Provisioner/local-exec.tf
+++ Terraform/File-Provisioner/local-exec.tf
@@ -5,4 +5,10 @@ resource "aws_instance" "example" {
   provisioner "local-exec" {
     command = "echo ${self.public_ip} >> ip.txt" #-------------> it will be printed in local system
   }
+
+  # we can have multiple part
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo Instance ${self.id} destroyed >> log.txt"
+  }
 }
