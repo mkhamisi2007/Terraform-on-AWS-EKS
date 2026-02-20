e40d59122dbcbf257670fbf50abcd79fe3a98d7d
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-20 17:15:59 +0100

    Update complete.tf

--- Terraform/File-Provisioner/complete.tf
+++ Terraform/File-Provisioner/complete.tf
@@ -9,17 +9,37 @@ resource "aws_instance" "demo" {
 
   vpc_security_group_ids = ["sg-xxxxxxxx"]
 
- 
   provisioner "local-exec" {
-    command = "echo EC2 is being created >> log.txt"
+    command = "echo EC2 ${self.id} is being created >> local_log.txt"
   }
 
- 
+  provisioner "file" {
+    source      = "app.sh"
+    destination = "/home/ubuntu/app.sh"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("my-key.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "file" {
+      source      = "config.json"
+      destination = "/home/ubuntu/config.json"
+  
+      connection {
+        type        = "ssh"
+        user        = "ubuntu"
+        private_key = file("my-key.pem")
+        host        = self.public_ip
+      }
+    }
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update",
-      "sudo apt install -y nginx",
-      "sudo systemctl start nginx"
+      "chmod +x /home/ubuntu/app.sh",
+      "/home/ubuntu/app.sh"
     ]
 
     connection {
@@ -30,9 +50,8 @@ resource "aws_instance" "demo" {
     }
   }
 
-
   provisioner "local-exec" {
     when    = destroy
-    command = "echo EC2 is being destroyed >> log.txt"
+    command = "echo EC2 ${self.id} destroyed >> local_log.txt"
   }
 }
