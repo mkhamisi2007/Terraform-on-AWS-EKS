d1fe9e33c161fdbed0dd5d86c5b0ef4e2ab6095d
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-27 11:34:11 +0100

    Add files via upload

---
+++ EKS/Basic-EKS/001-main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "mohammad-khamisi-us-bucket"      #-------> S3 bucket name (shoud be exist)
+    key    = "Terraform/eks/terraform.tfstate" #----> folder & file name
+    region = "us-east-1"
+
+    # For State Locking
+    dynamodb_table = "my-test-table-for-terraform" # -----------> DynamoDB name created below (Partition key = LockID(String) )
+  }
+}
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = var.aws_region
+}
+
