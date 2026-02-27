5f64b4d35f1d50b53afb8e03691a966304a0c686
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-02-27 12:09:23 +0100

    Update 106-eks-output.tf

--- EKS/Basic-EKS/106-eks-output.tf
+++ EKS/Basic-EKS/106-eks-output.tf
@@ -6,19 +6,9 @@ resource "null_resource" "kubeconfig" {
     aws_eks_node_group.eks_public_nodegroup
   ]
 
-  /*
+ 
   provisioner "local-exec" {
     command = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.eks_cluster.name} --kubeconfig ${path.module}\\kubeconfig_${aws_eks_cluster.eks_cluster.name}"
-  }
-*/
-  provisioner "local-exec" {
-    command = <<EOT
-    aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.eks_cluster.name} --kubeconfig ${path.module}\\kubeconfig_${aws_eks_cluster.eks_cluster.name}
-    $env:KUBECONFIG = "$PWD\kubeconfig_my_eks_cluster"
-    set-alias k kubectl
-    EOT
-
-    interpreter = ["PowerShell", "-Command"]
   }
 
   triggers = {
@@ -31,4 +21,5 @@ output "cluster_id" {
   description = "EKS Cluster ID"
   value       = aws_eks_cluster.eks_cluster.id
 }
+
 
