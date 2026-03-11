0c1eecda344422174069ecc6f308dcc7458ed514
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-03-11 14:34:51 +0100

    Add files via upload

---
+++ EKS/DNS/202-helm-lb.tf
@@ -0,0 +1,50 @@
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+resource "helm_release" "loadbalancer_controller" {
+  depends_on = [aws_iam_role.lbc_iam_role, time_sleep.wait_for_eks_access]
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set = [
+    {
+      name  = "image.repository"
+      value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/amazon/aws-load-balancer-controller"
+      # Changes based on Region : https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller" # exist in 201 file line 29
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = aws_iam_role.lbc_iam_role.arn
+    },
+    {
+      name  = "region"
+      value = var.aws_region
+    },
+    {
+      name  = "vpcId"
+      value = module.vpc.vpc_id
+    },
+    {
+      name  = "clusterName"
+      value = aws_eks_cluster.eks_cluster.name
+    }
+  ]
+
+}
